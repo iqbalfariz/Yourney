801a45e92f3132a1fcaaca4d87d9ea766dde1ff7
Fariz5 <iqball.frz@gmail.com>
2022-05-12 10:15:48 +0700

    Half Login Activity

--- app/src/main/java/com/izo/yourney/ui/MainActivity.kt
+++ app/src/main/java/com/izo/yourney/ui/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.izo.yourney.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.izo.yourney.R
+import com.izo.yourney.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn_login)
+
+        btn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
