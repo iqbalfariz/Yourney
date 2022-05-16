4ae2163842484ac85570a2c555b219a5835dc53a
Fariz5 <iqball.frz@gmail.com>
2022-05-16 20:59:20 +0700

    Add half about one to about three

---
+++ app/src/main/java/com/izo/yourney/ui/persona/AboutThreeActivity.kt
@@ -0,0 +1,12 @@
+package com.izo.yourney.ui.persona
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.izo.yourney.R
+
+class AboutThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_three)
+    }
+}
--- app/src/main/java/com/izo/yourney/ui/persona/PersonaActivity.kt
+++ app/src/main/java/com/izo/yourney/ui/persona/PersonaActivity.kt
@@ -1,12 +1,26 @@
 package com.izo.yourney.ui.persona
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.izo.yourney.R
+import com.izo.yourney.databinding.ActivityPersonaBinding
+import com.izo.yourney.ui.login.LoginActivity
 
 class PersonaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPersonaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_persona)
+        binding = ActivityPersonaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            val intent = Intent(this, AboutOneActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
