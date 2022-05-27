4e2d37d2da28d02b3095d998a80de80e2614a977
Fariz5 <iqball.frz@gmail.com>
2022-05-27 14:41:33 +0700

    Add intro counseling

---
+++ app/src/main/java/com/izo/yourney/ui/counseling/intro/IntroCounselingActivity.kt
@@ -0,0 +1,12 @@
+package com.izo.yourney.ui.counseling.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.izo.yourney.R
+
+class IntroCounselingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_counseling)
+    }
+}
