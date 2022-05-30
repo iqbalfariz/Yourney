0980a189860838a9a7ba9f00bd2f886afc8b026d
Fariz5 <iqball.frz@gmail.com>
2022-05-30 09:49:07 +0700

    Repair position in register page

---
+++ app/src/main/java/com/izo/yourney/ui/counseling/menu/MenuCounselingActivity.kt
@@ -0,0 +1,12 @@
+package com.izo.yourney.ui.counseling.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.izo.yourney.R
+
+class MenuCounselingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_counseling)
+    }
+}
