5be0ed3bacd4002e3af5f9cbefa855d2cbad56b5
unknown <audreyhoran39@gmail.com>
2022-05-31 22:08:07 +0700

    bla

---
+++ app/src/main/java/com/izo/yourney/ui/launchscreen/LaunchScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.izo.yourney.ui.launchscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.izo.yourney.R
+
+class LaunchScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch_screen)
+    }
+}
