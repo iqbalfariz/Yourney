be0d4866295938e2184037fa79a7de99e5e8ff6a
unknown <audreyhoran39@gmail.com>
2022-06-12 16:07:45 +0700

    hihi

---
+++ app/src/main/java/com/izo/yourney/ui/dailycheckin/DailyActivity.kt
@@ -0,0 +1,11 @@
+package com.izo.yourney.ui.dailycheckin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DailyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daily)
+    }
+}
