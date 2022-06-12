20d003e105cd636c8e17611610d03d850f081ed4
Fariz5 <iqball.frz@gmail.com>
2022-06-12 16:23:29 +0700

    Make sure

--- app/src/main/java/com/izo/yourney/ui/main/MainActivity.kt
+++ app/src/main/java/com/izo/yourney/ui/main/MainActivity.kt
@@ -63,7 +63,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mainViewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var bottomNavView : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,46 +73,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 //        clickEvents()
 
         supportActionBar?.hide()
-        bottomNavView = binding.BottomNavigationView
 
-        val homeFragment = HomeFragment()
-        val jelajahFragment = JelajahFragment()
-        val iconFragment = IconFragment()
-        val jadwalFragment = JadwalFragment()
-        val profileFragment = ProfileFragment()
-
-        setThatFragment(homeFragment)
-
-        bottomNavView.setOnItemReselectedListener {
-            when (it.itemId) {
-                R.id.beranda ->{
-                    setThatFragment(homeFragment)
-                }
-                R.id.jelajah ->{
-                    setThatFragment(jelajahFragment)
-                }
-                R.id.icon ->{
-                    setThatFragment(iconFragment)
-                }
-                R.id.jadwal ->{
-                    setThatFragment(jadwalFragment)
-                }
-                R.id.profile ->{
-                    setThatFragment(profileFragment)
-                }
-            }
-            true
-        }
 
     }
 
-    private fun setThatFragment(fragment : Fragment)  =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.frame,fragment)
-            commit()
-        }
 
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
+
+}
 
 //    private fun setupViewModel() {
 //        mainViewModel = ViewModelProvider(
@@ -147,66 +117,66 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 //
 //    }
 
-    private fun showDialog() {
-        val dialog = Dialog(this)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.custom_dialog_logout)
-
-
-        val btnYes = dialog.findViewById<Button>(R.id.btn_yes)
-        val btnNo = dialog.findViewById<Button>(R.id.btn_no)
-
-        btnYes.setOnClickListener {
-            mainViewModel.logout()
-            val intentLogout = Intent(this, LaunchScreenActivity::class.java)
-            startActivity(intentLogout)
-            finish()
-            dialog.dismiss()
-        }
-
-        btnNo.setOnClickListener {
-            dialog.dismiss()
-        }
-
-        dialog.show()
-    }
-
-    override fun onClick(v: View?) {
-        saveData()
-    }
-
-    private fun saveData() {
-        // wajib isi
-        val textName = name.text.toString().trim()
-        val textEmail = email.text.toString().trim()
-        val textPass = pasword.text.toString().trim()
-        val textConfirmPass = confirmPass.text.toString().trim()
-
-        if (textName.isEmpty()){
-            name.error = "Name is empty"
-            return
-        }
-
-        if (textEmail.isEmpty()){
-            email.error = "Email is empty"
-            return
-        }
-
-
-        if (textPass.isEmpty()) {
-            pasword.error = "Password is empty"
-            return
-        }
-
-        if (textConfirmPass.isEmpty()){
-            confirmPass.error = "Confirm Password is empty"
-            return
-        }
-
-        if (textPass != textConfirmPass){
-            Toast.makeText(applicationContext,"Password dan Konfirmasi Password tidak sama", Toast.LENGTH_SHORT).show()
-        }
+//    private fun showDialog() {
+//        val dialog = Dialog(this)
+//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        dialog.setCancelable(false)
+//        dialog.setContentView(R.layout.custom_dialog_logout)
+//
+//
+//        val btnYes = dialog.findViewById<Button>(R.id.btn_yes)
+//        val btnNo = dialog.findViewById<Button>(R.id.btn_no)
+//
+//        btnYes.setOnClickListener {
+//            mainViewModel.logout()
+//            val intentLogout = Intent(this, LaunchScreenActivity::class.java)
+//            startActivity(intentLogout)
+//            finish()
+//            dialog.dismiss()
+//        }
+//
+//        btnNo.setOnClickListener {
+//            dialog.dismiss()
+//        }
+//
+//        dialog.show()
+//    }
+//
+//    override fun onClick(v: View?) {
+//        saveData()
+//    }
+//
+//    private fun saveData() {
+//        // wajib isi
+//        val textName = name.text.toString().trim()
+//        val textEmail = email.text.toString().trim()
+//        val textPass = pasword.text.toString().trim()
+//        val textConfirmPass = confirmPass.text.toString().trim()
+//
+//        if (textName.isEmpty()){
+//            name.error = "Name is empty"
+//            return
+//        }
+//
+//        if (textEmail.isEmpty()){
+//            email.error = "Email is empty"
+//            return
+//        }
+//
+//
+//        if (textPass.isEmpty()) {
+//            pasword.error = "Password is empty"
+//            return
+//        }
+//
+//        if (textConfirmPass.isEmpty()){
+//            confirmPass.error = "Confirm Password is empty"
+//            return
+//        }
+//
+//        if (textPass != textConfirmPass){
+//            Toast.makeText(applicationContext,"Password dan Konfirmasi Password tidak sama", Toast.LENGTH_SHORT).show()
+//        }
 
         // tidak wajib isi
 //        val textcity = city.text.toString().trim()
@@ -237,6 +207,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 //            }
 //        }
 
-    }
-    // testing
-}
+//    }
+//    // testing
+//}
